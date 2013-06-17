ebffa0ecc8a6bea41f9bd7143d89661502cb2b05
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-17 04:50:24 +0000

    flip to prod settings

--- careermate/settings.py
+++ careermate/settings.py
@@ -67,7 +67,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'https://maggeraj.s3.amazonaws.com/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -178,8 +178,8 @@ AWS_SECRET_ACCESS_KEY = 'QR7SEpnipb/8YcU/II5Q/E6EjE++6h+mAYFrGfrv'
 AWS_STORAGE_BUCKET_NAME = 'maggeraj'
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
