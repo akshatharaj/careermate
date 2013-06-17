b1b0eb27c7e01f47c1eba63fcab5c8a9ab75875a
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-17 05:36:15 +0000

    notify listeners on new review response

--- careermate/settings.py
+++ careermate/settings.py
@@ -183,3 +183,12 @@ DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SERVER_EMAIL = 'admin@careermate.com'
+
+import os
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
--- cmapp/utils.py
+++ cmapp/utils.py
@@ -1,6 +1,22 @@
+from django.db.models import Q
+from django.core.mail import send_mail
+
 def notify_all_response_listeners(post_response):
     """
     given a response object, this method emails all other earlier responders 
     on the same post who chose to be notified when a new response is submitted
     """
-    pass
+    post = post_response.post
+    listeners = post.postresponse_set.filter(~(Q(id=post_response.id)), 
+                                             created__lt=post_response.created,
+                                             email_follow_up=True)
+    listeners = map(lambda x: x.author.email, listeners)
+    if listeners:
+        email_subject = 'New comment received on the review you subscribed to on Careermate'
+        body_template = 'cmapp/email_template/new_response_notice.html'
+        send_mail(email_subject, render_to_string(body_template, {'post': post}),
+                  settings.SERVER_EMAIL, listeners, fail_silently=True)
+
+
+
+
