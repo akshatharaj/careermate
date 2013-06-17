9508e62ff6b00efd0486018902afcba0a662d24d
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-17 22:59:58 +0000

    flip to prod db settings

--- careermate/settings.py
+++ careermate/settings.py
@@ -67,7 +67,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'https://maggeraj.s3.amazonaws.com/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -178,19 +178,17 @@ AWS_SECRET_ACCESS_KEY = 'QR7SEpnipb/8YcU/II5Q/E6EjE++6h+mAYFrGfrv'
 AWS_STORAGE_BUCKET_NAME = 'maggeraj'
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 SERVER_EMAIL = 'admin@careermate.com'
 
-"""
 import os
 EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-"""
